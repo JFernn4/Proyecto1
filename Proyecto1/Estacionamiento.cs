390b6394da1f1af403bc9c1f5b60360ae81669bc
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-08-27 23:42:55 -0600

    Clase "Estacionamiento"

---
+++ Proyecto1/Estacionamiento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1
+{
+    internal class Estacionamiento
+    {
+        public int espaciosDisponibles { get; set; }
+
+        public Estacionamiento(int espaciosDisponibles)
+        {
+            this.espaciosDisponibles = espaciosDisponibles;
+        }
+    }
+}
